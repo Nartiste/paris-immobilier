7d12edd42d8dd427f6810125851db1f6b36ae562
Nartiste <aladuc@hotmail.fr>
2026-04-28 01:14:36 +0200

    Fix : TopNav rendue 2x sur la home (déjà dans layout)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import HomeClient from "@/components/HomeClient";
 import CityFooter from "@/components/CityFooter";
 import HomeShell from "@/components/HomeShell";
-import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
-    <>
-      <TopNav />
-      <HomeClient
-        leftContent={<HomeShell />}
-        footerContent={<CityFooter />}
-      />
-    </>
+    <HomeClient
+      leftContent={<HomeShell />}
+      footerContent={<CityFooter />}
+    />
   );
 }
